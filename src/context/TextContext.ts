1ca13645c630c1d10d22e5a19edc49b72b0ba221
MariaShalaginova <pro_m_@mail.ru>
2025-03-25 21:23:48 +0300

    feat: добавлен контекст

---
+++ src/context/TextContext.ts
@@ -0,0 +1,16 @@
+import { createContext } from "react";
+import { TextLabelingProps } from "../Types/types";
+import { MOCK_LABELS, MOCK_TEXT } from "../mock";
+
+export const defaultContextValue: TextLabelingProps = {
+  text: MOCK_TEXT,
+  labels: MOCK_LABELS,
+  labeling: [],
+  selectedLabel: null,
+  selectLabel: () => {},
+  addTextLabel: () => {},
+};
+
+// Экспортируем только контекст
+export const TextContext =
+  createContext<TextLabelingProps>(defaultContextValue);
