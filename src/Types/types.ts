5e78d940059b2421ce82e3f6add3c86214c30e5d
MariaShalaginova <pro_m_@mail.ru>
2025-03-26 00:00:07 +0300

    refactor: рефактор типов

--- src/Types/types.ts
+++ src/Types/types.ts
@@ -7,14 +7,21 @@ export type TextLabel = {
   start: number; // начало фрагмента
   end: number; // конец фрагмента
   label: string; // метка
+  text: string;
 };
 
 export type TextLabelingProps = {
   labels: Label[];
   text: string;
   labeling: TextLabel[];
-  onChange?: (newLabeling: TextLabel[]) => void;
+  // onChange?: (newLabeling: TextLabel[]) => void;
   selectedLabel: string | null;
   addTextLabel: (start: number, end: number, selectedText: string) => void;
   selectLabel: (label: string) => void;
+  setLabeling: (labeling: TextLabel[]) => void;
 };
+
+export type HighlightedTextProps = Pick<
+  TextLabelingProps,
+  "text" | "labels" | "labeling"
+>;
